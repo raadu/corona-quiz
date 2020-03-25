0f72fa2c4fa579a9be5d1d5365decbfb8f62e4d4
raadu <raiyad.raad@gmail.com>
2020-03-26 00:03:21 +0600

    Updates

---
+++ precache-manifest.b1dd22727bdb645d12268770dc54d42c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8983038c181e32338336ca16d924a9",
+    "url": "/corona-quiz/index.html"
+  },
+  {
+    "revision": "3085cef02f15d00bfa13",
+    "url": "/corona-quiz/static/css/main.2f7f6b03.chunk.css"
+  },
+  {
+    "revision": "a160a5d0c8962a1ade9d",
+    "url": "/corona-quiz/static/js/2.5869807f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-quiz/static/js/2.5869807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3085cef02f15d00bfa13",
+    "url": "/corona-quiz/static/js/main.d7275c60.chunk.js"
+  },
+  {
+    "revision": "0c4461d1b22a2109cff1",
+    "url": "/corona-quiz/static/js/runtime-main.97fa42b7.js"
+  }
+]);
